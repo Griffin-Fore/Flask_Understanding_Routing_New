2574088ee98971fe590c17c36614c9c5c4b8eabf
Griffin Fore <griffinfore@gmail.com>
2023-08-26 21:34:14 -0700

    Redo of my second flask assignment creating routes

---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import Flask  # Import Flask to allow us to create our app
+app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+
+@app.route('/')          # The "@" decorator associates this route with the function immediately following
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/dojo')
+def dojo():
+    return 'Dojo!'
+
+@app.route('/say/<string:name>')
+def greeting(name):
+    return f"Hi {name}!"
+
+@app.route('/repeat/<int:count>/<string:word>')
+def repeatword(count, word):
+    return f"{count * word}"
+
+@app.route('/<string:invalid>')
+def invalidroute(invalid):
+    return "Sorry! No response. Try again."
+
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True)    # Run the app in debug mode.
+
